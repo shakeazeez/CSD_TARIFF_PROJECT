c792bd669b6ec5ac1f3dcb384d7072a3c567f067
Yong Huey <huey.yong.2024@smu.edu.sg>
2025-08-30 14:40:19 +0800

    Update App.jsx

--- frontend/tarrif-project/src/App.jsx
+++ frontend/tarrif-project/src/App.jsx
@@ -1,33 +1,127 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [dataset, setDataset] = useState([]);
+  const WTO_KEY = import.meta.env.VITE_WTO_API_KEY;
+
+  const fetchReview = async () => {
+    try {
+      const getReview = await axios.get(`https://api.wto.org/timeseries/v1/data?i=HS_A_0010&r=840&p=156&ps=2020-2022&pc=HS2&mode=full&subscription-key=${WTO_KEY}`);
+      console.log("GET Success", getReview);
+      setDataset(getReview.data.Dataset);
+      console.log(dataset);
+
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  const getIndicators = async () => {
+    try {
+      const getReview = await axios.get(`https://api.wto.org/timeseries/v1/indicators?i=all&t=all&pc=all&tp=all&frq=all&lang=1&subscription-key=${WTO_KEY}`);
+      console.log("GET Success", getReview);
+      setDataset(getReview.data);
+      console.log(dataset);
+
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  const getPC = async () => {
+    try {
+      const getReview = await axios.get(`https://api.wto.org/timeseries/v1/products?pc=all&subscription-key=${WTO_KEY}`);
+      console.log("GET Success", getReview);
+      setDataset(getReview.data);
+      console.log(dataset);
+
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  const getReporting = async () => {
+    try {
+      const getReview = await axios.get(`https://api.wto.org/timeseries/v1/reporters?ig=all&reg=all&gp=all&subscription-key=${WTO_KEY}`);
+      console.log("GET Success", getReview);
+      setDataset(getReview.data);
+      console.log(dataset);
+
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  const getPartner = async () => {
+    try {
+      const getReview = await axios.get(`https://api.wto.org/timeseries/v1/partners?ig=all&reg=all&gp=all&subscription-key=${WTO_KEY}`);
+      console.log("GET Success", getReview);
+      setDataset(getReview.data);
+      console.log(dataset);
+
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  const headers = [...new Set(dataset.flatMap(obj => Object.keys(obj)))];
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>heh</p>
+        <button onClick={fetchReview}>FetchReview 🔍</button>
+        <button onClick={getIndicators}>Indicators 🔍</button>
+        <button onClick={getPC}>PC 🔍</button>
+        <button onClick={getReporting}>Reporting 🔍</button>
+        <button onClick={getPartner}>Partners 🔍</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div style={{ padding: "20px" }}>
+      <h2>API Data Table</h2>
+      <table style={{ borderCollapse: "collapse", width: "100%" }}>
+        <thead>
+          <tr>
+            {headers.map((key) => (
+              <th
+                key={key}
+                style={{
+                  border: "1px solid #ddd",
+                  padding: "8px",
+                  backgroundColor: "#171414ff",
+                  textAlign: "left"
+                }}
+              >
+                {key}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {dataset.map((row, idx) => (
+            <tr key={idx}>
+              {headers.map((key) => (
+                <td
+                  key={key}
+                  style={{
+                    border: "1px solid #ddd",
+                    padding: "8px"
+                  }}
+                >
+                  {row[key] ?? ""}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
     </>
   )
 }
