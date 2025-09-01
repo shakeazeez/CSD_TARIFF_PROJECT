63ce649245e2870cece2de55b453746bd9645787
God3Tier <76461231+God3Tier@users.noreply.github.com>
2025-09-01 20:22:00 +0800

    file structure for tariffCalc of backend

---
+++ backend/tariffCalc/src/main/java/com/tariff/calculation/tariffCalc/TariffCalcApplication.java
@@ -0,0 +1,13 @@
+package com.tariff.calculation.tariffCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariffCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TariffCalcApplication.class, args);
+	}
+
+}
