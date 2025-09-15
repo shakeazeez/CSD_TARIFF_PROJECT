ab750ed287aa16588fe8b211471b3baca72fa882
God3Tier <76461231+God3Tier@users.noreply.github.com>
2025-09-15 17:07:30 +0800

    added cors for v1

--- backend/tariffCalc/src/main/java/com/tariff/calculation/tariffCalc/TariffCalcApplication.java
+++ backend/tariffCalc/src/main/java/com/tariff/calculation/tariffCalc/TariffCalcApplication.java
@@ -2,7 +2,9 @@ package com.tariff.calculation.tariffCalc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
@@ -17,5 +19,15 @@ public class TariffCalcApplication {
 		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
 		SpringApplication.run(TariffCalcApplication.class, args);
 	}
-
+	
+    @Bean 
+    public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NotNull CorsRegistry registry) {
+				registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173");
+			}
+		};
+	}
 }
