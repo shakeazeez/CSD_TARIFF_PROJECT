4726ddfc53c0f7c02d6892020fda4d0642d4b8a8
Shin En <shinen.wong.2024@computing.smu.edu.sg>
2025-09-22 21:53:29 +0800

    added entity GeneralUser

---
+++ backend/user/src/main/java/com/user/generalUser/GeneralUser.java
@@ -0,0 +1,45 @@
+package com.user.generalUser;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "general_user")
+public class GeneralUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String hashedPassword;
+    
+    @ElementCollection
+    @CollectionTable(name = "user_history", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "history_value")
+    private List<Integer> history;
+    
+    @ElementCollection
+    @CollectionTable(name = "user_tariff_ids", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "tariff_id")
+    private List<Integer> tariffIds;
+
+    public GeneralUser(String username, String hashedPassword, List<Integer> history, List<Integer> tariffIds) {
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+        this.history = history;
+        this.tariffIds = tariffIds;
+    }
+}
