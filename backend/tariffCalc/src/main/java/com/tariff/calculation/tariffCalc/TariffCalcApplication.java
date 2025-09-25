4b35e0b445001c20d4dc32e5f753916bfb287a55
God3Tier <josephyau2005@outlook.com>
2025-09-25 17:14:50 +0800

    test edit

--- backend/tariffCalc/src/main/java/com/tariff/calculation/tariffCalc/TariffCalcApplication.java
+++ backend/tariffCalc/src/main/java/com/tariff/calculation/tariffCalc/TariffCalcApplication.java
@@ -21,14 +21,5 @@ public class TariffCalcApplication {
 		SpringApplication.run(TariffCalcApplication.class, args);
 	}
 	
-    @Bean 
-    public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(@NotNull CorsRegistry registry) {
-				registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173");
-			}
-		};
-	}
+
 }
