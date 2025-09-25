ee679ee4c192a6e619681fd68d35c589a50f9a9e
God3Tier <josephyau2005@outlook.com>
2025-09-25 16:32:17 +0800

    seperated controller for login and general user functions

---
+++ backend/user/src/main/java/com/user/controller/AuthUserController.java
@@ -0,0 +1,48 @@
+package com.user.controller;
+
+import com.user.dto.TokenDTO;
+import com.user.security.exception.ApplicationAuthenticationException;
+import com.user.service.AuthUserService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/auth")
+public class AuthUserController {
+    
+    private final AuthUserService authUserService;
+    
+    public AuthUserController(AuthUserService authUserService) {
+        this.authUserService = authUserService;
+    }
+    
+    
+    @PostMapping("/login")
+    public ResponseEntity<TokenDTO> login(@RequestParam String username, @RequestParam String password) { 
+        try {
+            TokenDTO login = authUserService.login(username, password);
+            return ResponseEntity.ok(login);
+        } catch(ApplicationAuthenticationException | IllegalArgumentException e) {
+            return ResponseEntity.status(401).build();
+        }
+    }
+    
+    
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestParam String username, @RequestParam String password, @RequestParam String role) {
+        
+        try {
+            authUserService.createUser(username, password, role);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(409).build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+        
+        return ResponseEntity.accepted().build();
+    }
+}
--- backend/user/src/main/java/com/user/controller/GeneralUserController.java
+++ backend/user/src/main/java/com/user/controller/GeneralUserController.java
@@ -3,16 +3,11 @@ package com.user.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.user.dto.TokenDTO;
-import com.user.generalUser.GeneralUser;
-import com.user.security.exception.ApplicationAuthenticationException;
 import com.user.service.AuthUserService;
 import com.user.service.GeneralUserService;
 
@@ -20,11 +15,9 @@ import com.user.service.GeneralUserService;
 @RestController
 public class GeneralUserController {
     private final GeneralUserService generalUserService;
-    private final AuthUserService authUserService;
 
-    public GeneralUserController(GeneralUserService generalUserService, AuthUserService authUserService) {
+    public GeneralUserController(GeneralUserService generalUserService) {
         this.generalUserService = generalUserService;
-        this.authUserService = authUserService;
     }
 
     @PostMapping("/{userId}/pinned-tariffs/{tariffId}")
@@ -47,31 +40,6 @@ public class GeneralUserController {
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
         }
-    }
-    
-    @PostMapping("/login")
-    public ResponseEntity<TokenDTO> login(@RequestParam String username, @RequestParam String password) { 
-        try {
-            TokenDTO login = authUserService.login(username, password);
-            return ResponseEntity.ok(login);
-        } catch(ApplicationAuthenticationException | IllegalArgumentException e) {
-            return ResponseEntity.status(401).build();
-        }
-    }
-    
-    
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestParam String username, @RequestParam String password, @RequestParam String role) {
-        
-        try {
-            authUserService.createUser(username, password, role);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(409).build();
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
-        
-        return ResponseEntity.accepted().build();
     }
     
     // @GetMapping("/hi/details")
