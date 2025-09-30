0f09d62ceefc8347e99bd8273ec34d6cfab1ba75
rynrindesu <jingxi.ee.2024@computing.smu.edu.sg>
2025-09-30 18:52:26 +0800

    changes to swagger ui

--- backend/tariffCalc/src/main/java/com/tariff/calculation/tariffCalc/controller/AdminController.java
+++ backend/tariffCalc/src/main/java/com/tariff/calculation/tariffCalc/controller/AdminController.java
@@ -3,22 +3,22 @@ package com.tariff.calculation.tariffCalc.controller;
 import com.tariff.calculation.tariffCalc.dto.TariffDeleteDTO;
 import com.tariff.calculation.tariffCalc.service.CrudService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.media.Content;
+// import io.swagger.v3.oas.annotations.Operation;
+// import io.swagger.v3.oas.annotations.responses.ApiResponse;
+// import io.swagger.v3.oas.annotations.responses.ApiResponses;
+// import io.swagger.v3.oas.annotations.tags.Tag;
+// import io.swagger.v3.oas.annotations.media.Content;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/tariff/admin")
-@Tag(name = "Admin Controller", description = "Admin tasks such as deleting items, countries and tariffs")
+//@RestController
+//@RequestMapping("/tariff/admin")
+//@Tag(name = "Admin Controller", description = "Admin tasks such as deleting items, countries and tariffs")
 public class AdminController {
     private final CrudService crudService;
     
@@ -26,11 +26,11 @@ public class AdminController {
         this.crudService = crudService;
     }
     
-    @Operation(summary = "Delete item by ID")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "Item deleted", content = @Content()),
-        @ApiResponse(responseCode = "400", description = "Bad request", content = @Content())
-    })
+    // @Operation(summary = "Delete item by ID")
+    // @ApiResponses({
+    //     @ApiResponse(responseCode = "200", description = "Item deleted", content = @Content()),
+    //     @ApiResponse(responseCode = "400", description = "Bad request", content = @Content())
+    // })
     @DeleteMapping("/item/{itemId}")
     public ResponseEntity<?> deleteItem(@PathVariable int itemId) {
         try {
@@ -41,11 +41,11 @@ public class AdminController {
         }
     }
 
-    @Operation(summary = "Delete country by ID")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "Country deleted", content = @Content()),
-        @ApiResponse(responseCode = "400", description = "Bad request", content = @Content())
-    })
+    // @Operation(summary = "Delete country by ID")
+    // @ApiResponses({
+    //     @ApiResponse(responseCode = "200", description = "Country deleted", content = @Content()),
+    //     @ApiResponse(responseCode = "400", description = "Bad request", content = @Content())
+    // })
     @DeleteMapping("/country/{countryId}")
     public ResponseEntity<?> deleteCountry(@PathVariable int countryId) {
         try {
@@ -56,16 +56,16 @@ public class AdminController {
         }
     }
     
-    @Operation(summary = "Delete tariff by DTO")
-    @io.swagger.v3.oas.annotations.parameters.RequestBody(
-        description = "DTO for tariff deletion",
-        required = true,
-        content = @Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = TariffDeleteDTO.class))
-    )
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "Tariff deleted", content = @Content()),
-        @ApiResponse(responseCode = "400", description = "Bad request", content = @Content())
-    })
+    // @Operation(summary = "Delete tariff by DTO")
+    // @io.swagger.v3.oas.annotations.parameters.RequestBody(
+    //     description = "DTO for tariff deletion",
+    //     required = true,
+    //     content = @Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = TariffDeleteDTO.class))
+    // )
+    // @ApiResponses({
+    //     @ApiResponse(responseCode = "200", description = "Tariff deleted", content = @Content()),
+    //     @ApiResponse(responseCode = "400", description = "Bad request", content = @Content())
+    // })
     @DeleteMapping("/tariff")
     public ResponseEntity<?> deleteItem(@RequestBody TariffDeleteDTO tariffDeleteDTO) {
         try {
