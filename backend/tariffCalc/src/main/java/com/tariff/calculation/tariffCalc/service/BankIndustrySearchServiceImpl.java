b1bcfc9ace0f15447e98955ddd41f6bcf9ff6d68
rynrindesu <jingxi.ee.2024@computing.smu.edu.sg>
2025-10-21 21:00:02 +0800

    post mapping that returns list of items of selected industry with at least one tariff entry available

--- backend/tariffCalc/src/main/java/com/tariff/calculation/tariffCalc/service/BankIndustrySearchServiceImpl.java
+++ backend/tariffCalc/src/main/java/com/tariff/calculation/tariffCalc/service/BankIndustrySearchServiceImpl.java
@@ -14,9 +14,11 @@ import com.tariff.calculation.tariffCalc.dto.bankServiceDto.TariffItemFilterDTO;
 import com.tariff.calculation.tariffCalc.item.ItemRepo;
 import com.tariff.calculation.tariffCalc.tariff.Tariff;
 import com.tariff.calculation.tariffCalc.tariff.TariffRepo;
+import com.tariff.calculation.tariffCalc.country.CountryRepo;
 import com.tariff.calculation.tariffCalc.item.Item;
 import com.tariff.calculation.tariffCalc.category.Industry;
 import com.tariff.calculation.tariffCalc.country.Country;
+import com.tariff.calculation.tariffCalc.service.TariffCalculationImpl;
 
 
 @Service
@@ -24,12 +26,14 @@ public class BankIndustrySearchServiceImpl implements BankIndustrySearchService 
     
     private final ItemRepo itemRepo;
     private final TariffRepo tariffRepo;
+    private final CountryRepo countryRepo;
 
     private final Logger log = LoggerFactory.getLogger(BankIndustrySearchServiceImpl.class);
 
-    public BankIndustrySearchServiceImpl (ItemRepo itemRepo, TariffRepo tariffRepo) {
+    public BankIndustrySearchServiceImpl (ItemRepo itemRepo, TariffRepo tariffRepo, CountryRepo countryRepo) {
         this.itemRepo = itemRepo;
         this.tariffRepo = tariffRepo;
+        this.countryRepo = countryRepo;
     }
 
     /* 
@@ -40,26 +44,32 @@ public class BankIndustrySearchServiceImpl implements BankIndustrySearchService 
         int startYear = LocalDate.parse(itemFilterDTO.startDate()).getYear();
         int endYear = LocalDate.parse(itemFilterDTO.endDate()).getYear();
 
+        String countryName = itemFilterDTO.homeCountry();
         Industry industry = Industry.valueOf(itemFilterDTO.industry());
         List<Item> preItemList = itemRepo.findByIndustry(industry); 
 
         List<Item> filteredItemList = new ArrayList<>();
 
+        // log.info("Items to be printed" + preItemList.toString());
+
         // items that users can select should at least have 1 entry of tariff details for the date range
         for (Item item: preItemList) {
-            List<Tariff> tariffList = item.getTariffs();
+            Country reportingCountry =  countryRepo.findByCountryName(countryName)
+                                        .orElseThrow(() -> new IllegalArgumentException("Country not found."));
 
-            log.info("tariffList" + tariffList.toString() + "\n\n\n\n\n\n\n");
+            // log.info("Reporting country FOUND: " +reportingCountry.toString());
+
+            List<Tariff> tariffList = tariffRepo.findByReportingCountryAndItem(reportingCountry, item);
+
+            // log.info("tariffList" + tariffList.toString() + "\n\n\n\n\n\n\n");
+
             for (Tariff tariff: tariffList) {
-
-                String reportingCountry = tariff.getReportingCountry().getCountryName();
                 int yearReported = tariff.getLocalDate().getYear();
 
                 boolean isWithinYearRange = startYear <= yearReported && yearReported <= endYear;
-                boolean isSelectedCountry = reportingCountry.equals(itemFilterDTO.homeCountry());
 
                 // at least need to have one tariff imposed on the item
-                if (isSelectedCountry && isWithinYearRange) {
+                if (isWithinYearRange) {
                     filteredItemList.add(item);
                     break;
                 }
